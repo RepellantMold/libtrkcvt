05c29e337a47fcf248dc205d88217feac7d527c8
RepellantMold <repellantmold@outlook.com>
2024-03-23 12:43:41 -0700

    wait hang on i screwed up-

--- src/main.c
+++ src/main.c
@@ -4,12 +4,26 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
+    int return_value = EXIT_SUCCESS;
+    FILE *STMfile;
+    FILE *S3Mfile;
     /* printf("Hello, World!\n"); */
-	
-	if (argc != 3)
-		printf("usage: %s <input.s3m> <output.stm>", argv[0]);
-	
-	
-	
-    return EXIT_SUCCESS;
+    
+    if (argc != 3)
+        printf("usage: %s <input.s3m> <output.stm>", argv[0]);
+    
+    S3Mfile = fopen(argv[1], "rb");
+    STMfile = fopen(argv[2], "wb");
+    
+    if (S3Mfile == NULL || STMfile == NULL)
+        goto closefiledescriptors;
+    
+    /* more code soon */
+    
+    closefiledescriptors:
+    fclose(S3Mfile);
+    fclose(STMfile);
+
+    returndasvalue:
+    return return_value;
 }
