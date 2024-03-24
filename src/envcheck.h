084cd893f300a879ab2cfc151a09eecf913e095e
RepellantMold <repellantmold@outlook.com>
2024-03-23 18:05:10 -0700

    fix linking

---
+++ src/envcheck.h
@@ -0,0 +1,5 @@
+#include <limits.h>
+
+#if CHAR_BIT != 8
+#error "a byte has to be 8-bits!"
+#endif
--- src/pattern.c
+++ src/pattern.c
@@ -7,6 +7,10 @@
 
 void convert_effect(unsigned char effect, unsigned char parameter) {
   switch (effect) {
+    default:
+      puts("WARNING: unknown effect!");
+      break;
+
     /* no effect */
     case 0:
       break;
@@ -29,9 +33,48 @@ void convert_effect(unsigned char effect, unsigned char parameter) {
         parameter = 0;
       }
       break;
+
+    /* volume slide */
+    case 4:
+      if ((parameter & 0xF0) || (parameter & 0x0F)) 
+        puts("WARNING: there's no fine volume slides!");
+      goto noeffectmemory;
+      break;
+
+    /* porta up/down */
+    case 6:
+    case 5:
+      if (parameter >= 0xE0)
+        puts("WARNING: there's no fine porta up/down!");
+      goto noeffectmemory;
+      break;
+
+    /* tone porta */
+    case 7:
+      goto noeffectmemory;
+      break;
+    
+    /* vibrato */
+    case 8:
+      puts("WARNING: vibrato depth is doubled compared to other trackers.");
+      goto noeffectmemory;
+      break;
+
+    /* tremor */
+    case 9:
+      goto noeffectmemory;
+      break;
+
+    /* arpeggio */
+    case 10:
+      goto noeffectmemory;
+      break;
   }
 
   return;
 
-  noeffectmemory: if (!parameter) puts("WARNING: there's no effect memory, this will be treated as a no-op.");
+  noeffectmemory:
+  if (!parameter)
+    puts("WARNING: there's no effect memory, this will be treated as a no-op.");
+  return;
 }
