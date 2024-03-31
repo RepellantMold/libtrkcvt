d7981c7cef9ee9e008422b621b19ec4b0fb3084f
RepellantMold <repellantmold@outlook.com>
2024-03-31 14:09:05 -0700

    move the crc functions into header

--- src/sample.h
+++ src/sample.h
@@ -10,7 +10,4 @@ typedef struct {
 void convert_unsigned_to_signed(u8* pcm, usize length);
 usize calculate_sample_padding(size_t sample_size);
 
-u32 crc32_for_byte(u32 byte);
-u32 crc32(const unsigned char *input, usize size);
-
 #endif
