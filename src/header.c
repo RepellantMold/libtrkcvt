cd40b16b4f365a1e4d86750295e66812c37e7737
RepellantMold <repellantmold@outlook.com>
2024-03-31 17:32:42 -0700

    e

--- src/header.c
+++ src/header.c
@@ -56,11 +56,10 @@ void grab_s3m_parapointers(FILE* file) {
 
   /* see section "2.6 Load Order Data" from "FireLight S3M Player Tutorial.txt" */
   for(count = 0; count < order_count; count++) {
-    optional_printf("Order %u -> %u\n", (int)count, (int)i);
+    optional_printf("Order %zu -> %zu\n", count, i);
     if (s3m_order_array[count] < S3M_ORDER_MARKER) {
-      s3m_order_array[i] = s3m_order_array[count];
-      i++;
-      if(s3m_order_array[count] > pattern_count) 
+      s3m_order_array[i++] = s3m_order_array[count];
+      if(s3m_order_array[count] > pattern_count)
         pattern_count = s3m_order_array[count];
     }
   }
@@ -69,14 +68,14 @@ void grab_s3m_parapointers(FILE* file) {
   (void)!fread(s3m_inst_pointers, sizeof(u16), sample_count, file);
 
   for(i = 0; i < sample_count; i++) {
-    optional_printf("Sample %u:\n", (u8)i);
+    optional_printf("Sample %zu:\n", i);
     s3m_inst_pointers[i] = (u16)convert_from_parapointer(s3m_inst_pointers[i]);
   }
 
   (void)!fread(s3m_pat_pointers, sizeof(u16), pattern_count, file);
 
   for(i = 0; i < pattern_count; i++) {
-    optional_printf("Pattern %u:\n", (u8)i);
+    optional_printf("Pattern %zu:\n", i);
     s3m_pat_pointers[i] = (u16)convert_from_parapointer(s3m_pat_pointers[i]);
   }
 }
