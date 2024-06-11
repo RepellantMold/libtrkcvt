6c94d8667436104861c141a37f0bd32ceb2fefa5
Jade A. <repellantmold@outlook.com>
2024-06-11 15:43:12 -0700

    prepare for using structs

--- include/file.h
+++ include/file.h
@@ -3,6 +3,11 @@
 
 #include <stdio.h>
 
+u8 fgetb(FILE* fp);
 u16 fgetw(FILE* fp);
+u32 fgetl(FILE* fp);
+
+u16 fputw(u16 value, FILE* fp);
+u32 fputl(u32 value, FILE* fp);
 
 #endif
--- src/file.c
+++ src/file.c
@@ -3,12 +3,49 @@
 #include "envcheck.h"
 #include "ext.h"
 
-// a helper from https://github.com/viiri/st2play!
+u8 fgetb(FILE* fp) {
+  return (u8)fgetc(fp);
+}
+
+// a helper from https://github.com/viiri/st2play
 u16 fgetw(FILE* fp) {
   u8 data[2];
 
-  data[0] = (u8)fgetc(fp);
-  data[1] = (u8)fgetc(fp);
+  data[0] = fgetb(fp);
+  data[1] = fgetb(fp);
 
   return (data[1] << 8) | data[0];
+}
+
+// another helper from https://github.com/viiri/st2play
+u32 fgetl(FILE *fp)
+{
+	u8 data[4];
+
+	data[0] = fgetb(fp);
+	data[1] = fgetb(fp);
+	data[2] = fgetb(fp);
+	data[3] = fgetb(fp);
+
+	return (data[3] << 24) | (data[2] << 16) | (data[1] << 8) | data[0];
+}
+
+u16 fputw(u16 value, FILE* fp) {
+  u8 data[2];
+
+  data[0] = value & 0xFF;
+  data[1] = value >> 8;
+
+  return fwrite(data, sizeof(u8), 2, fp);
+}
+
+u32 fputl(u32 value, FILE* fp) {
+  u8 data[4];
+
+  data[0] = value & 0xFF;
+  data[1] = (value >> 8) & 0xFF;
+  data[2] = (value >> 16) & 0xFF;
+  data[3] = value >> 24;
+
+  return fwrite(data, sizeof(u8), 4, fp);
 }
